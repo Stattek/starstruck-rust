a7fdeddc3b4573ca79b3b4fbc441e2486957c630
stattek <alienhuntsgames@gmail.com>
2024-05-05 04:26:16 -0500

    very basic stuff being added. very tired

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 mod entities;
+mod game;
 
 use entities::*;
+use game::game_loop;
 
 fn main() {
     println!("Starstruck is back once again.");
+
+    let player = Player::new("test".to_string(), 100, 50, 10, 1, 0);
+
+    // play the game
+    game_loop(&player);
 }
