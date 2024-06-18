9871e496c11b4a6577849c79479b0e51a46e3d36
Stattek <stattek1@pm.me>
2024-06-17 20:58:40 -0500

    Fixing up broken code and my earlier poor design choices

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,23 @@ mod game;
 
 use colored::Colorize;
 use entities::*;
-use game::game_loop;
+use game::GameState;
 
 fn main() {
     println!("{}", "Starstruck is back once again.".bold().blue());
 
     let mut player = Player::new("test".to_string(), Stats::new(10, 10, 10), 1, 0, false);
+    
+    // TODO: temporary code possibly
+    if true {
+        let mut players = Vec::new();
+        players.push(player);
 
-    // play the game
-    game_loop(&mut player);
+        let enemies = Vec::new();
+
+        let mut the_game = GameState::new(players, enemies);
+
+        // play the game
+        the_game.game_loop();
+    }
 }
