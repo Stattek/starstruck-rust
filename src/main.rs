4ac61cca7284bf186aff702e4cbfcb24543c1761
Stattek <alienhuntsgames@gmail.com>
2024-07-12 23:01:54 -0500

    Adding more features - Entities can now generate random attack damage - Added more entity checks, but these aren't working properly yet.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use game::GameState;
 fn main() {
     println!("{}", "Starstruck is back once again.".bold().blue());
 
-    let player = Player::new("test".to_string(), Stats::new(10, 10, 10), 1, 0, false);
+    let player = Player::new("test".to_string(), Stats::new(10, 10, 10, 10), 1, 0, false);
 
     // TODO: temporary code possibly
     if true {
