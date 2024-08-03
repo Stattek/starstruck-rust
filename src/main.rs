ff63ac3df82f054d7162a2e77392e20e97dd0922
Stattek <stattek1@pm.me>
2024-08-02 20:27:15 -0500

    Print logo text.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,17 @@ use entity_components::{entity::Entity, player::Player, stats::Stats};
 use game::GameState;
 
 fn main() {
-    println!("{}", "Starstruck is back once again.".bold().blue());
+    println!(
+        "{}",
+        "  _____ _                 _                   _
+ / ____| |               | |                 | |
+| (___ | |_ __ _ _ __ ___| |_ _ __ _   _  ___| | __
+ \\___ \\| __/ _` | '__/ __| __| '__| | | |/ __| |/ /
+ ____) | || (_| | |  \\__ \\ |_| |  | |_| | (__|   <
+|_____/ \\__\\__,_|_|  |___/\\__|_|   \\__,_|\\___|_|\\_\\
+"
+        .blue()
+    );
 
     let player = Player::new("test".to_string(), Stats::new(10, 10, 10, 10), 1, 0, false);
 
